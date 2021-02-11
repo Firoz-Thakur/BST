7110fc51799136a21416ec5165d9e04def5d04ea
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2021-02-11 05:54:23 +0530

    Create Lca of BST.cpp

---
+++ Lca of BST.cpp
@@ -0,0 +1,15 @@
+https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-bst/1#
+
+
+Node* LCA(Node *root, int n1, int n2)
+{
+ if(root==NULL)
+  return root;
+  
+ if((n1<root->data )&& (n2<root->data))
+   return LCA(root->left,n1,n2);
+ if((n1>root->data) && (n2>root->data))
+  return LCA(root->right,n1,n2);
+  return root;
+
+}
